ab090f2289c6b7c02d62f67655dca08431f5c6e6
MahmoudAliKhider <mahmoudkhider37@gmail.co>
2023-12-11 21:08:50 +0200

    add a Sign out fincionality

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -4,11 +4,14 @@ import search from './../../images/icons/searchIcon.png';
 import shoppingCart from './../../images/icons/shopping-cart.png';
 import { Link } from 'react-router-dom';
 import { useAuth } from '../../context/GlobalState';
+import { auth } from './../../firebase';
 
 import './Header.css'
 const Header = () => {
   const { user } = useAuth();
-
+  const handelAuthintication = () => {
+    auth.signOut();
+  }
   return (
     <div className='header'>
       <Link to='/'>
@@ -19,11 +22,11 @@ const Header = () => {
         <img className="header-searchIcon" src={search} alt='Search_image' />
       </div>
 
-      <div className='header-nav'>
-        <Link to='/login'>
-          <div className='header-option'>
+      <div className='header-nav' >
+        <Link to={!user && '/login'}>
+          <div className='header-option' onClick={handelAuthintication}>
             <div className='header-optionLineOne'>Hello {user ? `${user.email}` : "Guest"} </div>
-            <div className='header-optionLinetwo'>Sign In</div>
+            <div className='header-optionLinetwo'>{user ? `SignOut` : "Sign In"}</div>
           </div>
         </Link>
 
